a4243970ee2340a541b754224c58e5681c04cf00
GL-19 <loiolagl19@gmail.com>
2021-11-09 01:31:08 -0300

    Adicionado typescript à aplicação

---
+++ src/components/RepositoryItem/index.tsx
@@ -0,0 +1,19 @@
+interface RepositoryItemProps {
+	repository: {
+		name: string;
+		description: string;
+		html_url: string;
+	};
+}
+export function RepositoryItem(props: RepositoryItemProps) {
+	const { repository } = props;
+	return (
+		<li>
+			<strong>{repository?.name}</strong>
+			<p>{repository?.description}</p>
+			<a href={repository?.html_url} target="_blank">
+				Acessar repositório
+			</a>
+		</li>
+	);
+}
